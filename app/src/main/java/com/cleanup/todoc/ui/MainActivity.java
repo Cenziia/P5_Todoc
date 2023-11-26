e1c859458e29c21506b35198e5dc7fd3539d863e
Cynthia NOEL <cynthia.noel.r@gmail.com>
2023-11-26 16:05:44 +0100

    tests 1

--- app/src/main/java/com/cleanup/todoc/ui/MainActivity.java
+++ app/src/main/java/com/cleanup/todoc/ui/MainActivity.java
@@ -2,7 +2,6 @@ package com.cleanup.todoc.ui;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
